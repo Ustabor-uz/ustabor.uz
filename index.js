ba29833c4ff46dc14b926ba6e19bccb8caf770ac
shokhweb07 <shokhweb@gmail.com>
2023-08-18 15:31:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+function dropdown1(){
+    document.querySelector('.svg1').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg2').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text1').classList.toggle('quiz-text-active')
+}
+
+function dropdown2(){
+    document.querySelector('.svg3').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg4').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text2').classList.toggle('quiz-text-active')
+}
+
+function dropdown3(){
+    document.querySelector('.svg5').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg6').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text3').classList.toggle('quiz-text-active')
+}
+
+function dropdown4(){
+    document.querySelector('.svg7').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg8').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text4').classList.toggle('quiz-text-active')
+}
+
+function dropdown5(){
+    document.querySelector('.svg9').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg10').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text5').classList.toggle('quiz-text-active')
+}
+
+function dropdown6(){
+    document.querySelector('.svg11').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg12').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text6').classList.toggle('quiz-text-active')
+}
+
+function dropdown7(){
+    document.querySelector('.svg13').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg14').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text7').classList.toggle('quiz-text-active')
+}
+
+function dropdown8(){
+    document.querySelector('.svg15').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg16').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text8').classList.toggle('quiz-text-active')
+}
+
+function dropdown9(){
+    document.querySelector('.svg17').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg18').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text9').classList.toggle('quiz-text-active')
+}
+
+function dropdown10(){
+    document.querySelector('.svg19').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg20').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text10').classList.toggle('quiz-text-active')
+}
+
+function dropdown11(){
+    document.querySelector('.svg21').classList.toggle('quiz-svg1-active')
+    document.querySelector('.svg22').classList.toggle('quiz-svg2-active')
+    document.querySelector('.quiz-text11').classList.toggle('quiz-text-active')
+}
